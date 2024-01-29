380c8d67cc66f23bfdaa477aee3fa4e640eb27a3
uditsangule <uditsangule@gmail.com>
2024-01-29 14:20:41 +0530

    revert: masks minrect removed on segmentation view

--- Code/main.py
+++ Code/main.py
@@ -60,7 +60,7 @@ def streamVideo(videopath, outputpath ,args):
             if conf > 0.8 :imp.save_(image=frame_roi , outputh=imp.Fstatus(args.output+os.sep+os.path.basename(videopath).split('.',-1)[0] + os.sep + '{0}_{1}'.format(trackid,label) + os.sep) + str(fno))
             imp.showim(image=frame_[bbox[1]:bbox[3], bbox[0]:bbox[2], :], windowname=str(trackid)+'_' + str(label))
 
-            mask_ = imp.putconts(mask_ , contours=tracker[0].masks.xy[i_] ,minrect=True if clsid in minrect else False , color=uniqcols[clsid],filled=True)
+            mask_ = imp.putconts(mask_ , contours=tracker[0].masks.xy[i_]  , color=uniqcols[clsid],filled=True)
             frame_ = imp.putconts(frame_ , contours=tracker[0].masks.xy[i_] ,minrect=True if clsid in minrect else False ,color=uniqcols[clsid],filled=False)
             frame_ = imp.puttext(image=frame_ , pos=(int(bbox[0]+bbox[2])//2 , int(bbox[1]+bbox[3])//2) , text=label , color=tuple(uniqcols[clsid].tolist()))
             print(f"Clip:{os.path.basename(videopath)} - Timestamp:{imp.timedelta(milliseconds=cap.get(cv2.CAP_PROP_POS_MSEC))} - Object:{label} - Confidence:{np.round(100*conf,2)}%")
